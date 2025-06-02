f71ed44639b91a9db6871f89be5a022afef2a6ae
Ahmed Alshatebi <154013344+a7med-alshatebi@users.noreply.github.com>
2025-06-02 23:48:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ const backendUrl = 'http://localhost:3000/instagram';
 
 */
 
-const backendUrl = 'https://t4l.a7med-alshatebi.tech';
+const backendUrl = 'https://t4l.a7med-alshatebi.tech/instagram';
 let swiper = null; // This should be the only swiper declaration
 
 async function fetchInstagramPosts() {
