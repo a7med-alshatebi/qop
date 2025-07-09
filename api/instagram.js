b1fe003807bd82e6e52417431d40e47296e094be
Ahmed Alshatebi <154013344+a7med-alshatebi@users.noreply.github.com>
2025-07-09 18:49:51 +0300

    Update instagram.js

--- api/instagram.js
+++ api/instagram.js
@@ -1,13 +1,9 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-module.exports = async (req, res) => {
-    try {
-        const response = await fetch(
-            `https://graph.instagram.com/me/media?fields=media_url,caption,media_type,permalink&access_token=${process.env.IG_ACCESS_TOKEN}`
-        );
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch posts' });
-    }
-}
+export default async (req, res) => {
+  const response = await fetch(
+    `https://graph.instagram.com/me/media?fields=media_url,caption&access_token=${process.IG_ACCESS_TOKEN}`
+  );
+  const data = await response.json();
+  res.status(200).json(data);
+};
