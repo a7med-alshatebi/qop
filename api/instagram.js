a9b6bdd3395b0828f7346a1573b0cda50ab4cbf6
Ahmed Alshatebi <154013344+a7med-alshatebi@users.noreply.github.com>
2025-07-09 18:55:14 +0300

    Update instagram.js

--- api/instagram.js
+++ api/instagram.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 export default async (req, res) => {
   const response = await fetch(
-    `https://graph.instagram.com/me/media?fields=media_url,caption&access_token=${process.IG_ACCESS_TOKEN}`
+    `https://graph.instagram.com/me/media?fields=media_url,caption&access_token=${process.env.IG_ACCESS_TOKEN}`
   );
   const data = await response.json();
   res.status(200).json(data);
